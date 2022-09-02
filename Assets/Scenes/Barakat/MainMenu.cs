1edb258083f5fb4cf276a3b0efee71ce87408bb9
Barakat <mbarakat2010@gmail.com>
2022-09-02 23:57:12 +0200

    Implementation of Instruction and Exit buttons

--- Assets/Scenes/Barakat/MainMenu.cs
+++ Assets/Scenes/Barakat/MainMenu.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MainMenu : MonoBehaviour { 
+public class MainMenu : MonoBehaviour {
 
-    public void PlayGame ()
-{
+    public void PlayGame()
+    {
         SceneManager.LoadScene(1);
-}
-
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 
 
 }
